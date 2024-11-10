46852ffb54641e7175400d1ba69cd1a6b60539d1
shikeik <2667097598@qq.com>
2024-11-11 01:01:07 +0800

    初始化项目

---
+++ src/main/java/com/goldsprite/log/core/Main.java
@@ -0,0 +1,8 @@
+package com.goldsprite.log.core;
+
+public class Main
+{
+	public static String getGreeting(){
+		return "Hello Worrrrrrrrrld!";
+	}
+}
---
+++ src/main/java/com/goldsprite/log/samples/Main.java
@@ -0,0 +1,12 @@
+package com.goldsprite.log.samples;
+
+import com.goldsprite.log.core.Main;
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+		String str = Main.getGreeting();
+		System.out.println(str);
+		
+    }
+}
